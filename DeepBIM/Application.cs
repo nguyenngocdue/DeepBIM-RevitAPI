effd6f015182a44aab507b0c2c238165e6b57de9
nguyenngocdue <duengocnguyen@gmail.com>
2025-07-20 12:18:19 +0700

    create projectAdd project files.

---
+++ DeepBIM/Application.cs
@@ -0,0 +1,26 @@
+﻿using DeepBIM.Commands;
+using Nice3point.Revit.Toolkit.External;
+
+namespace DeepBIM
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "DeepBIM");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/DeepBIM;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/DeepBIM;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
---
+++ DeepBIM/Commands/StartupCommand.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+
+namespace DeepBIM.Commands
+{
+    /// <summary>
+    ///     External command entry point
+    /// </summary>
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class StartupCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+        }
+    }
+}
