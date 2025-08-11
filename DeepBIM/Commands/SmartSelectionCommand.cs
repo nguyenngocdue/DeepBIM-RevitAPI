b4c833f08d99534b40877f74f5922938f1966bdd
nguyenngocdue <duengocnguyen@gmail.com>
2025-08-11 22:32:28 +0700

    add ui smartSelection

---
+++ DeepBIM/Commands/SmartSelectionCommand.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using System.Windows.Interop;
+
+namespace DeepBIM.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class SmartSelectionCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                // ✅ Lấy danh sách đã chọn
+                var selectedIds = uidoc.Selection.GetElementIds();
+
+                // Khởi tạo Window + ViewModel
+                var win = new DeepBIM.Views.SmartSelectionWindow(); // Pass 'uiapp' as required by the constructor
+                win.DataContext = new DeepBIM.ViewModels.SmartSelectionViewModel(doc, uidoc, selectedIds);
+
+                // Đặt owner là Revit để form modal và luôn trên cùng
+                new WindowInteropHelper(win) { Owner = uiapp.MainWindowHandle };
+
+                win.ShowDialog();   // hoặc .Show() nếu muốn non‑modal
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ DeepBIM/ViewModels/SmartSelectionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.UI;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace DeepBIM.ViewModels
+{
+
+
+    public class SmartSelectionViewModel : INotifyPropertyChanged
+    {
+        public SmartSelectionViewModel(Document doc, UIDocument uidoc, ICollection<ElementId> selectedIds = null)
+        {
+        }
+
+        event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+       
+}
